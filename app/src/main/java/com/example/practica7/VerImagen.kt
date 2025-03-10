5b855b001383a29f297f0dc1304436ab4f7802fa
Josue Belmont <89788489+josuebelmont@users.noreply.github.com>
2025-03-10 17:55:42 -0600

    ya quedo

--- app/src/main/java/com/example/practica7/VerImagen.kt
+++ app/src/main/java/com/example/practica7/VerImagen.kt
@@ -2,7 +2,7 @@ package com.example.practica7
 
 import android.os.Bundle
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,8 +10,8 @@ import androidx.core.view.WindowInsetsCompat
 class VerImagen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_ver_imagen)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -19,9 +19,20 @@ class VerImagen : AppCompatActivity() {
         }
 
         val cimagen2 = findViewById<ImageView>(R.id.gimagen2)
+        val tvTitulo = findViewById<TextView>(R.id.tvTitulo)
+        val tvDescripcion = findViewById<TextView>(R.id.tvDescripcion)
 
-        val pelicula:Pelicula = intent.getSerializableExtra("peli") as Pelicula
-        cimagen2.setImageResource(pelicula.imagen)
-
+        // Obtener la película enviada
+        val pelicula = intent.getSerializableExtra("peli") as? Pelicula
+        if (pelicula != null) {
+            cimagen2.setImageResource(pelicula.imagen)
+            tvTitulo.text = pelicula.titulo
+            tvDescripcion.text = pelicula.detalle
+        } else {
+            // En caso de error, mostrar valores predeterminados
+            cimagen2.setImageResource(R.drawable.imagen3)
+            tvTitulo.text = "Película no encontrada"
+            tvDescripcion.text = "No hay información disponible"
+        }
     }
 }
